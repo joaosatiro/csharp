fc610fab5251ea9fbb0ba582de258cb2e0d38b8d
Joao Satiro <joao.v.satiro@gmail.com>
2022-02-22 22:24:59 -0300

    migrations

--- Dotnet 6/API/Program.cs
+++ Dotnet 6/API/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/user", () => "Sátiro!");
@@ -55,7 +58,36 @@ public static class ProductRepository {
     }
 }
 
+public class Category {
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Tag {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+}
+
 public class Product {
+    public int Id { get; set; }
     public string Code { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }    
+    public List<Tag> Tags { get; set; }    
+}
+
+public class ApplicationDbContext : DbContext {
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>().Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+        builder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
+        builder.Entity<Product>().Property(p => p.Code).HasMaxLength(20).IsRequired();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@Sql2019;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
 }
