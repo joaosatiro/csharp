5d3aa6b17d3a4d551b99079177d5df4eddd1c017
Joao Satiro <joao.v.satiro@gmail.com>
2022-03-03 21:53:13 -0300

    Add...

---
+++ Dotnet 6/src/Domain/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product : Entity
+{
+    public string Name { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool Stock { get; set; }
+    public bool Active { get; set; } = true;
+}
---
+++ Dotnet 6/src/Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,24 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static async Task<IResult> Action(CategoryRequest categoryRequest, HttpContext http, ApplicationDbContext context)
+    {
+        var category = new Category
+        {
+            Name = categoryRequest.Name,
+        };
+
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
+
+        return Results.Created($"/categories/{category.Id}", category.Id);
+    }
+}
