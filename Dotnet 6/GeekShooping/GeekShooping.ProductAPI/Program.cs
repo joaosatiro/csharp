defb749b8d6313f6466d92632e2275b9782cb92e
Joao Satiro <joao.v.satiro@gmail.com>
2022-08-10 21:35:18 -0300

    Add...

--- Dotnet 6/GeekShooping/GeekShooping.ProductAPI/Program.cs
+++ Dotnet 6/GeekShooping/GeekShooping.ProductAPI/Program.cs
@@ -1,7 +1,13 @@
+using GeekShooping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connection = builder.Configuration["MySQLConnection:MySWLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 25))));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
